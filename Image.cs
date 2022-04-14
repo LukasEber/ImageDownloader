7df17bcc0ec0a0424592190f54bf3750e1cffb8f
Lukas Eber <lukaseber9@gmail.com>
2022-04-14 15:10:07 +0200

    Add project files.

---
+++ Image.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using DownloadImageFromWeb;
+
+namespace DownloadImageFromWeb
+{
+    public class Image
+    {
+        public string URL { get; private set; }
+        public string Filename { get; private set; }
+
+        public void DownloadNewImage()
+        {
+            GetUrlFromUser();
+            NewImage();
+        }
+
+        private void GetUrlFromUser()
+        {
+            Console.WriteLine("Enter url: ");
+            this.URL = Console.ReadLine();
+            Console.WriteLine("Enter filename(without filetype): ");
+            this.Filename = Console.ReadLine();
+        }
+
+        private void NewImage()
+        {
+            try
+            {
+                SaveImage();
+            }
+            catch (System.Net.WebException)
+            {
+                Console.WriteLine("Invalid url.");
+            }
+            catch (System.Runtime.InteropServices.ExternalException)
+            {
+                Console.WriteLine("Invalid url.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void SaveImage()
+        {
+            Bitmap image = LoadImageFromWeb(@URL);
+            image.Save($@"C:\Users\Public\Pictures\{Filename}.png", ImageFormat.Png);
+            Console.WriteLine($@"Image saved at C:\Users\Public\Pictures\{Filename}.png");
+        }
+
+        static Bitmap LoadImageFromWeb(string url)
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(url);
+            Bitmap image = new Bitmap(stream);
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+
+            return image;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DownloadImageFromWeb;
+
+namespace LoadImageFromWeb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string addImage = "y";
+            while (addImage == "y" || addImage == "Y")
+            {
+                Image newImage = new Image();
+                newImage.DownloadNewImage();
+
+                Console.Write("Wanna save a new image (y/n): ");
+                addImage = Console.ReadLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
